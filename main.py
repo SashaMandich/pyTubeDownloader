166e97059c3d225607e38f8b57dab09ca74e42d5
sasha <sasazd@hotmail.com>
2023-12-06 08:43:01 +0000

    added home/user/downloads path

--- main.py
+++ main.py
@@ -1,8 +1,11 @@
 from pytube import YouTube
+from pathlib import Path
 
-def download_720p_mp4_videos(url: str, outpath: str = "./"):
+path = str(Path.home())+'/downloads'
+
+def download_720p_mp4_videos(url: str, outpath: path):
     yt = YouTube(url)
     yt.streams.filter(file_extension="mp4").get_by_resolution("720p").download(outpath)
 
 if __name__ == "__main__":
-    download_720p_mp4_videos("https://www.youtube.com/watch?v=SyF5Hpwo3WA&t=6s")
+    download_720p_mp4_videos("https://www.youtube.com/watch?v=FDBqs7GOY_w", path)
