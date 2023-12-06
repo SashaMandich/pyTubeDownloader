510db734b125708c4610980ddba26aae345713df
sasha <sasazd@hotmail.com>
2023-12-06 09:19:32 +0000

    fix

--- main.py
+++ main.py
@@ -1,11 +1,24 @@
 from pytube import YouTube
 from pathlib import Path
 
-path = str(Path.home())+'/downloads'
+# url array (list)
+url_array = ["https://www.youtube.com/watch?v=Pjw2A3QU8Qg", 
+             "https://www.youtube.com/watch?v=YanYDTGkLH8", 
+             "https://www.youtube.com/watch?v=XqZsoesa55w", 
+             "https://www.youtube.com/watch?v=pZw9veQ76fo"]
 
-def download_720p_mp4_videos(url: str, outpath: path):
-    yt = YouTube(url)
-    yt.streams.filter(file_extension="mp4").get_by_resolution("720p").download(outpath)
+#change path to your desired location
+path = str(Path.home())+'/Downloads'
 
 if __name__ == "__main__":
-    download_720p_mp4_videos("https://www.youtube.com/watch?v=FDBqs7GOY_w", path)
+    # Using a loop to download each video
+    for value in url_array:
+        yt = YouTube(value)
+        file_name = yt.streams.filter(file_extension="mp4").get_by_resolution("720p").title
+        file_size_mb = yt.streams.filter(file_extension="mp4").get_by_resolution("720p").filesize_mb    
+
+        #download
+        yt.streams.filter(file_extension="mp4").get_by_resolution("720p").download(path)
+        
+        #print info
+        print("downloaded: "+str(file_name)+"; file_size_in_mb: "+str(file_size_mb))
